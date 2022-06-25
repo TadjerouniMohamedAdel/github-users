8b4bf2f98e72c28c380f212c488b5cf2a1bda9c9
adel <mohnagato@gmail.com>
2022-06-25 11:54:49 +0100

    config router with react-router-dom@6

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,15 @@
 import React from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import Error404 from './views/Error404/Error404';
+import User from './views/User/User';
+import Users from './views/Users/Users';
 
-const App = () => <div>App</div>;
+const App = () => (
+  <Routes>
+    <Route path="/" element={<Navigate to="/users" replace />} />
+    <Route path="/users" element={<Users />} />
+    <Route path="/users/:userid" element={<User />} />
+    <Route path="*" element={<Error404 />} />
+  </Routes>
+);
 export default App;
